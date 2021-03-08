0d922eb261187706193192309ebaaed345f90195
kzhou53 <kzhou22@amherst.edu>
2021-03-07 19:34:47 -0500

    add title

--- homeworks/ps3a_shiny/app.R
+++ homeworks/ps3a_shiny/app.R
@@ -4,13 +4,16 @@ ui <- fluidPage(
   sliderInput(inputId = "num", 
               label = "Choose a number", 
               value = 25, min = 1, max = 100),
+  textInput(inputId = "title", 
+            label = "Write a title",
+            value = "Histogram of Random Normal Values"),
   plotOutput("hist"),
   verbatimTextOutput("stats")
 )
 
 server <- function(input, output) {
   output$hist <- renderPlot({
-    hist(rnorm(input$num))
+    hist(rnorm(input$num), main = input$title)
   })
   output$stats <- renderPrint({
     summary(rnorm(input$num))
