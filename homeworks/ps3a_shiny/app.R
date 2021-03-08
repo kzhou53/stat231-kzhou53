a2b5593d1373da1723f1ffe92b6c46446f1b34e1
kzhou53 <kzhou22@amherst.edu>
2021-03-08 10:05:38 -0500

    added navlist panel

--- homeworks/ps3a_shiny/app.R
+++ homeworks/ps3a_shiny/app.R
@@ -7,20 +7,49 @@ ui <- fluidPage(
   textInput(inputId = "title", 
             label = "Write a title",
             value = "Histogram of Random Normal Values"),
-  plotOutput("hist"),
-  verbatimTextOutput("stats")
+  navlistPanel(              
+    tabPanel(title = "Normal data",
+             plotOutput("norm"),
+             actionButton("renorm", "Resample")
+    ),
+    tabPanel(title = "Uniform data",
+             plotOutput("unif"),
+             actionButton("reunif", "Resample")
+    ),
+    tabPanel(title = "Chi Squared data",
+             plotOutput("chisq"),
+             actionButton("rechisq", "Resample")
+    )
+  )
 )
 
 server <- function(input, output) {
-  output$hist <- renderPlot({
-    hist(rnorm(input$num), main = isolate({input$title}))
+  rv <- reactiveValues(
+    norm = rnorm(25), 
+    unif = runif(25),
+    chisq = rchisq(25, 2))
+  
+  observeEvent(input$renorm, { rv$norm <- rnorm(input$num) })
+  observeEvent(input$reunif, { rv$unif <- runif(input$num) })
+  observeEvent(input$rechisq, { rv$chisq <- rchisq(input$num, 2) })
+  
+  output$norm <- renderPlot({
+    hist(rv$norm(input$num), main = isolate({input$title}))
   })
   output$stats <- renderPrint({
     summary(rnorm(input$num))
   })
+  output$unif <- renderPlot({
+    hist(rv$unif, main = isolate({input$title}))
+  })
+  output$chisq <- renderPlot({
+    hist(rv$chisq, main = isolate({input$title}))
+  })  
 }
 
 shinyApp(ui = ui, server = server)
+
+
 
 #rv <- reactiveValues(
  # norm = rnorm(25), # wanted to do norm = rnorm(input$num) but didnt work
