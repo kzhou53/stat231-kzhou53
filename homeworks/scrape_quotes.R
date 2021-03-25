520576eb6bbc2977025d0f031a205d20c5855f70
kzhou53 <kzhou22@amherst.edu>
2021-03-24 22:57:05 -0400

    scrape website .r files

---
+++ homeworks/scrape_quotes.R
@@ -0,0 +1,15 @@
+alone_quotes <- read_html("https://www.brainyquote.com/topics/alone-quotes")
+
+quote <- alone_quotes %>%
+  html_nodes(".oncl_q") %>%
+  html_text()
+
+person <- quotes_html %>%
+  html_nodes(".oncl_a") %>%
+  html_text()
+
+quotes_dat <- data.frame(quote = quotes, stringsAsFactors = FALSE) %>%
+  filter(quote != "\n") %>%
+  mutate(person = person
+         , together = paste('"', as.character(quote), '" --'
+                            , as.character(person), sep=""))
